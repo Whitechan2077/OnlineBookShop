6b2eae226ea737e608d106cf6f5e3f66c735a475
Hoang Dung <dungbui8198@gmail.com>
2023-10-14 20:54:49 +0700

    xoa vi loi

--- src/main/java/com/workshop/bookstore/repositories/BookRepository.java
+++ src/main/java/com/workshop/bookstore/repositories/BookRepository.java
@@ -1,6 +1,5 @@
 package com.workshop.bookstore.repositories;
 
-import com.workshop.bookstore.entity.Sach;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
